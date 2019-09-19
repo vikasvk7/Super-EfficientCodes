b8fe16de777bdd44cb272cb19820b18b48cee7ef
Vikas <43094340+vikasvk7@users.noreply.github.com>
2019-09-19 23:53:58 +0530

    Add files via upload

---
+++ suffixArray.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<pair<string,int>> suffix;
+string suff[8] ;
+int index[8] ;
+//bool cmp(string a,string b){
+//	return a.compare(b);
+//}
+void search(char *pattern,char *word ,int n){
+	int m=strlen(pattern);
+	int beg=0 ;
+	int end = n-1;
+	while(beg<=end){
+		int mid= (beg+end)/2;
+		int ans = strncmp(pattern,word+index[mid],m);
+		if(ans==0){
+			cout<<"\nPattern found at index"<<index[mid] ;
+			return ;
+		}
+		if(ans<0) end=mid-1;
+		if(ans>0) beg=mid+1;
+	}
+	cout<<"Not found" ;
+}
+void buildSuffix(char *word,int n){
+	for(int i=0;i<n;i++){
+		suffix.push_back(make_pair(word+i,i));
+	}
+	sort(suffix.begin(),suffix.end());
+
+	for(int i=0;i<n;i++){
+		index[i] = suffix[i].second ;
+	}
+	for(int i=0;i<n;i++){
+		suff[i] = suffix[i].first ;
+	}
+	
+	//Indices
+	for(auto i : index){
+		cout<<i<<" " ;
+	}
+	cout<<"\n";
+	
+	//Suffix
+	for(auto i : suff){
+		cout<<i<<" " ;
+	}
+}
+int main(){
+	char word[] = "codechef";
+	int n=strlen(word) ;
+	buildSuffix(word,n);
+	char pattern[]= "ode";
+	search(pattern,word,n);
+}
